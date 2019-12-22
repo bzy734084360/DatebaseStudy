8d98fac2737eb2e39ed0430be6054351189170bf
bzy734084360 <bzy734084360@hotmail.com>
2019-12-22 22:20:53 +0800

    版本提交

--- SqlServerStudyTwo.sql
+++ SqlServerStudyTwo.sql
@@ -1,3 +1,4 @@
+/*
 --练习
 select *from StudentInfo;
 select *from ClassInfo;
@@ -21,7 +22,7 @@ values('数据库'),
 ('计算机英语'),
 ('算法与设计')
 --创建学生成绩表
-create  table Score
+create  table ScoreInfo
 (
 	scoreId	int identity(1,1) primary key not null,
 	stuId int not null,
@@ -30,9 +31,9 @@ create  table Score
 	foreign key (subId) references Subject(subId),
 	score int not null
 )
-select *from Score;
+select *from ScoreInfo;
 --添加学生成绩
-insert into Score
+insert into ScoreInfo
 values(5,1,80),
 (5,2,88),
 (5,3,90),
@@ -59,6 +60,145 @@ select top 2 * from StudentInfo ;
 select top 2 percent  *from StudentInfo;
 --Order by  asc|desc  升序|降序
 --升序
-select top 2 *from StudentInfo st order by st.StudentID asc;
+select  *from StudentInfo st order by st.StudentID asc;
 --降序
-select top 2*from StudentInfo st order by st.StudentID desc;
+select *from StudentInfo st order by st.StudentID desc;
+--消除重复行
+select distinct t.StudentGender from StudentInfo t;
+*/
+
+/*
+
+--条件查询 where
+select *from StudentInfo t 
+where t.StudentID>10;
+--between and
+--取出编号从3-8的学生
+select *from StudentInfo t 
+where t.StudentID between 3 and 8;
+select *from StudentInfo t 
+where t.StudentID>=3 and t.StudentID<=8;
+--获取成绩表 分数 在85到90之间的数据
+select *from ScoreInfo t 
+where t.score between 85 and 90;
+
+-- in
+--取班级编号为1-3的学生信息
+select *from StudentInfo t 
+where t.ClassID between 1 and 3;
+--取班级编号为1或3的学生信息
+select *from StudentInfo t 
+where t.ClassID in(1,3);
+--and or not 
+select *from StudentInfo t
+where t.ClassID=1 or t.ClassID=3;
+select *from StudentInfo t
+where t.ClassID>=1 and t.ClassID<=3;
+--去学生编号为3-8的在1班的学生
+select *from StudentInfo t 
+where  t.StudentID between 3 and 8
+and t.ClassID=1;
+--取不在1班的学生
+select *from StudentInfo t 
+where t.ClassID not in(1,3);
+--not
+select *from StudentInfo t 
+where not  (t.StudentID between 3 and 8
+or t.ClassID=1);--and or 会在not中做转换
+
+select *from StudentInfo t
+where t.StudentGender=0;
+--模糊查询
+--查询名字中包含三的学生
+select *from StudentInfo st
+where st.StudentName like '_三';
+select *from StudentInfo st
+where st.StudentName like '_三%';
+--查询姓张的学生
+select *from StudentInfo st
+where st.StudentName like '张%';
+--查询姓名为两个名字的姓张的学生
+select *from StudentInfo st
+where st.StudentName like '张_';
+--查询使用电话为13段的学生信息
+select *from StudentInfo st
+where st.StudentPhone like '13%';
+--
+select *from StudentInfo st
+where st.StudentPhone like '1[0-4]%';
+--
+select *from StudentInfo st
+where st.StudentPhone like '1[013]%';
+--查询出使用QQ邮箱的
+select *from StudentInfo st
+where st.StudentEmail like '%@qq.com';
+--将部分数据电话改为null
+update StudentInfo set StudentPhone =null where StudentID in (5,6);
+
+--not 分配律
+--not ( a and b)
+--=> not a or not b 
+--查询学生电话为null 的信息
+select *from StudentInfo st
+where st.StudentPhone is null;
+--查不为空的
+select *from StudentInfo st
+where st.StudentPhone is not  null;
+*/
+
+/*
+
+--连接查询
+--查询学生姓名及所在班级名称
+--内连接查询 inner join
+select st.StudentName,cl.ClassName 
+from StudentInfo st
+inner join ClassInfo cl
+on cl.ClassID=st.ClassID
+where cl.ClassID=1;
+--外连接 left join 以左表为基准
+select *from ClassInfo cl
+left join StudentInfo st
+on st.ClassID =cl.ClassID;
+--外连接 right join 以右表为基准
+select *from StudentInfo st
+right join ClassInfo cl
+on st.ClassID =cl.ClassID;
+--外连接 full join
+select *from StudentInfo st
+full join ClassInfo cl
+on st.ClassID =cl.ClassID;
+--练习
+--学生姓名、班级名称、科目名称、对应成绩
+select st.StudentName,cl.ClassName, sb.subTitle,sc.score from StudentInfo st
+inner join ScoreInfo sc on sc.stuId=st.StudentID
+inner join Subject sb on sb.subId=sc.subId
+inner join ClassInfo cl on cl.ClassID=st.ClassID;
+*/
+--聚合函数
+
+--count 计数函数
+--查询学生人数
+select COUNT(st.StudentPhone)班级人数 from StudentInfo st
+where st.ClassID=1;
+select COUNT(*)班级人数 from StudentInfo st
+--max 获取最大值
+--获取分数最高的学生
+select MAX(sc.score) from ScoreInfo sc
+where sc.subId=2;
+--min 获取最小值
+--获取学生编号为5 的最低分
+select min(sc.score) from ScoreInfo sc 
+where sc.stuId=5;
+--avg 平均成绩
+--求数据库科目的平均分
+select AVG(sc.score) from ScoreInfo sc 
+inner join Subject sb on sb.subId=sc.subId
+where sb.subTitle ='数据库';
+--sum 求和函数
+--求学生成绩的综合
+select SUM(sc.score) from ScoreInfo sc;
+--开窗函数 over
+--将统计信息分布到行中
+select  sc.*, avg(sc.score)
+from ScoreInfo sc;
