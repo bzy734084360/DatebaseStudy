6c5c79422a0486f3ce4f7bfca3417e4d0d6181ff
bzy734084360 <bzy734084360@hotmail.com>
2020-03-17 00:29:13 +0800

    新增5天学习

---
+++ FiveDay/_02封装SqlHelper/SqlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02封装SqlHelper
+{
+    /// <summary>
+    /// SqlServer数据库工具类
+    /// </summary>
+    public static partial class SqlHelper
+    {
+        private static string connStr = "server=.;database=Study;uid=sa;pwd=sa";
+    }
+}
