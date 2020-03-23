c2a5bdba84baf9963f9ae3254beb267591487dae
bzy734084360 <bzy734084360@hotmail.com>
2020-03-23 23:32:16 +0800

    xxxxx

--- SqlServerStudySix.sql
+++ SqlServerStudySix.sql
@@ -43,7 +43,36 @@ where a.rowindex between 5 and 8;
 --case when then end
 
 --查询学生名称、科目名称、成绩
-select *
+select st.studentName,si.score,sb.subTitle
 from ScoreInfo si
 inner join StudentInfo st on st.StudentID=si.stuId
 inner join Subject sb on sb.subId=si.subId
+
+-- 姓名 语文 数据 英语
+
+--列转行
+select a.StudentName,
+sum(a.数据库) 数据库,
+sum(a.软件工程) 软件工程,
+sum(a.计算机英语) 计算机英语,
+sum(a.算法与设计) 算法与设计
+from  (select   st.StudentName,
+case  sb.subTitle  when '数据库' then si.score   else 0 end  as 数据库,
+case  sb.subTitle  when '软件工程' then si.score   else 0 end  as 软件工程,
+case  sb.subTitle  when '计算机英语' then si.score   else 0 end  as 计算机英语,
+case  sb.subTitle  when '算法与设计' then si.score   else 0 end  as 算法与设计
+from ScoreInfo si
+inner join Subject sb on si.subID=sb.subID
+inner join StudentInfo st on st.StudentID=si.stuId) a 
+group by a.StudentName
+select *from ScoreInfo;
+select *from Subject;
+
+-- 班级 男生 女生
+select a.ClassName,
+case a.StudentGender  when  0 then a.Num else 0 end as 男,
+case a.StudentGender  when  1 then a.Num else 0 end as 女
+from  (select cl.ClassName,stu.StudentGender,Count(*)Num
+from StudentInfo stu
+inner join ClassInfo cl on cl.ClassID=stu.ClassID
+group by cl.ClassName,stu.StudentGender)a 
